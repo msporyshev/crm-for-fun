0ebfd81caaccc367b38d3cde2a481f6665073f32
Max Sporyshev <coockoombra@gmail.com>
2012-07-02 08:30:57 +1100

    Create new app with rails new command. Add twitter-bootstrap, will_paginate to Gemfile.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmApp::Application.config.secret_token = '9e91b155e9017d801e3651667e665771ee672b9f000b8ca0316ab385246a93dafdc61f148722c6858aa0a4b352d7c564272f5493fc8930f9fd4de8e796d00a61'
